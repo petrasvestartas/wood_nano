6cc009289565c9fa389c3a0abeb633f84157f250
petrasvestartas <petrasvestartas@gmail.com>
2026-05-11 17:22:58 +0200

    chore: pin session-py==0.84.0 in w_install

--- plugin_rhino/plugin/commands/w_install.py
+++ plugin_rhino/plugin/commands/w_install.py
@@ -1,9 +1,11 @@
 #! python3
 # venv: wood_env
-# r: wood-nano==1.0.7
+# r: wood-nano==1.0.8
+# r: session-py==0.84.0
 # r: session-rhino==0.37.0
 
 import wood_nano
 import session_rhino
+from importlib.metadata import version
 
-print("Successfully installed wood-nano==1.0.7 and session-rhino==0.37.0!")
+print(f"Successfully installed wood-nano=={version('wood-nano')} and session-rhino=={version('session-rhino')}!")
